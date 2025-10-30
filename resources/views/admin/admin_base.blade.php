855c6f72c7abbc5d978d926153d64a259a63a284
YASIR AHMAD <yazirahmed123@gmail.com>
2025-10-30 06:39:51 +0300

    modified Viet assets

--- resources/views/admin/admin_base.blade.php
+++ resources/views/admin/admin_base.blade.php
@@ -7,7 +7,9 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
-    <!-- Bootstrap CSS-->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- <!-- Bootstrap CSS-->
     <link rel="stylesheet" href="admin/vendor/bootstrap/css/bootstrap.min.css">
     <!-- Font Awesome CSS-->
     <link rel="stylesheet" href="admin/vendor/font-awesome/css/font-awesome.min.css">
@@ -23,7 +25,7 @@
     <link rel="shortcut icon" href="admin/img/favicon.ico">
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]--> --}}
   </head>
   <body>
     <header class="header">   
